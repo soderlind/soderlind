c112b969d72d7980e9be94e15fdf04f4b6de0334
Per Søderlind <per@soderlind.no>
2020-08-09 20:34:02 +0200

    List my latest WordPress plugins

--- README.md
+++ README.md
@@ -1,38 +1,33 @@
- Read [my blog](https://soderlind.no/) or [follow @soderlind on Twitter](https://twitter.com/soderlind).
+ Read [my blog](https://soderlind.no/) or [follow me on Twitter](https://twitter.com/soderlind).
 
-<table><tr><td valign="top" width="33%">
 
-### On my blog
-<!-- blog starts -->
-* [How to import native ES modules in WordPress](https://soderlind.no/how-to-import-native-es-modules-in-wordpress/) - 04.12.2019
-* [add_theme_support( 'disable_block_style')](https://soderlind.no/add-theme-support-disable-block-style/) - 07.11.2019
-* [Hide block styles in Gutenberg](https://soderlind.no/hide-block-styles-in-gutenberg/) - 18.10.2019
-* [Local by Flywheel: Using WordMove to mirror sites](https://soderlind.no/local-by-flywheel-using-wordmove-to-mirror-sites/) - 30.09.2019
-* [Learn to code!](https://soderlind.no/learn-to-code/) - 21.12.2018
-<!-- blog ends -->
-More on [soderlind.no](https://soderlind.no/)
-</td><td valign="top" width="34%">
 
-### Read
-<!-- read starts -->
-* [Why use F#?](https://fsharpforfunandprofit.com/why-use-fsharp) - 09.08.2020
-* [Add width and height attributes to  elements #6652](https://github.com/WordPress/gutenberg/issues/6652) - 08.08.2020
-* [6 Best Examples of Content Aggregator Websites That Rock](https://wpmayor.com/6-best-examples-content-aggregator-websites) - 08.08.2020
-* [Introducing Cloudinarys Gatsby Plugins](https://cloudinary.com/blog/introducing_cloudinary_s_gatsby_plugins) - 06.08.2020
-* [WordPress 5.5 Field Guide](https://make.wordpress.org/core/2020/07/30/wordpress-5-5-field-guide) - 06.08.2020
-<!-- read ends -->
-</td><td valign="top" width="33%">
+### My latest WordPress plugins
+<!-- plugins starts -->
+[**Read Offline allows you to download or print posts and pages. You can download the post as PDF, ePub and mobi**](https://github.com/soderlind/read-offline) &nbsp;&nbsp;&nbsp;
+[*Date Range field for Ninja Forms*](https://github.com/soderlind/date-range-ninja-forms) &nbsp;&nbsp;&nbsp;
+[**A PHP CodeSniffer sniff that checks if the global PHP functions are missing a backslash.**](https://github.com/soderlind/coding-standard) &nbsp;&nbsp;&nbsp;
+[*ES6 WordPress Ajax Demo*](https://github.com/soderlind/es6-wp-ajax-demo) &nbsp;&nbsp;&nbsp;
+[**Use WordPress Customizer to add JavaScript**](https://github.com/soderlind/additional-javascript) &nbsp;&nbsp;&nbsp;
+[*WP-DenyHost denies a spammer from accessing your WordPress blog*](https://github.com/soderlind/wp-denyhost) &nbsp;&nbsp;&nbsp;
+[**Ninja Forms Action for DSS Events**](https://github.com/soderlind/ninja-forms-dss-event-action) &nbsp;&nbsp;&nbsp;
+[*WordPress Customizer Toggle Control is a generic toggle control you can use to replace the checkbox control.*](https://github.com/soderlind/class-customizer-toggle-control) &nbsp;&nbsp;&nbsp;
+[**Activate ACF PRO license using WP CLI**](https://github.com/soderlind/wp-cli-acfpro-activate) &nbsp;&nbsp;&nbsp;
+[*Convert an WordPress Export File to PDF*](https://github.com/soderlind/wxr2pdf) &nbsp;&nbsp;&nbsp;
+[**WordPress Customizer Range Value Control is a generic range with value control you can use to replace the default range control.**](https://github.com/soderlind/class-customizer-range-value-control) &nbsp;&nbsp;&nbsp;
+[*An add-in that gives you full Polylang support in WordPress customizer.*](https://github.com/soderlind/customizer-polylang) &nbsp;&nbsp;&nbsp;
+[**Automatically Activate SearchWP on Subsite**](https://github.com/soderlind/ms-searchwp-subsite-activate) &nbsp;&nbsp;&nbsp;
+[*PHP Code Beautifier and Fixer for Visual Studio Code*](https://github.com/soderlind/vscode-phpcbf) &nbsp;&nbsp;&nbsp;
+[**The plugin makes it possible to link to a playlist item **](https://github.com/soderlind/wp-link-to-playlist-item) &nbsp;&nbsp;&nbsp;
+[*Demo plugin to show how to implement your custom permalink for your WordPress plugin.*](https://github.com/soderlind/my-permalink-demo) &nbsp;&nbsp;&nbsp;
+[**Cloud2PNG captures snapshots of any website using the Cloudinary URL2PNG add-on**](https://github.com/soderlind/cloud2png) &nbsp;&nbsp;&nbsp;
+[*WordPress Debug Bar, List Script & Style Dependencies*](https://github.com/soderlind/debug-bar-list-dependencies) &nbsp;&nbsp;&nbsp;
+[**WordPress shortcode that creates a Delicious tag cloud or a list of tags**](https://github.com/soderlind/delicious-tagroll-shortcode) &nbsp;&nbsp;&nbsp;
+[*Replace the default Metro Share social icons with icomoon social fonts*](https://github.com/soderlind/metro-share-social-fonts) &nbsp;&nbsp;&nbsp;
+<!-- plugins ends -->
 
-### Tweets
-<!-- tweet starts -->
-* [.. dvs, KS2-en ser valg av annen leverandør enn Epic som en trussel](https://twitter.com/user/status/1292114854321545218) - 08.08.2020
-* [@ChristinGorman Ser ut som du har "gjettet" rett:"Dersom Epic ikke velges som hovedleverandør til Akson, kan de b… https://t.co/LD6GL9FDFo](https://twitter.com/user/status/1292114852736106497) - 08.08.2020
-* [https://t.co/PDMG2AY84O](https://twitter.com/user/status/1292106198305497088) - 08.08.2020
-* [Times are changing #WAP https://t.co/7Rwk1omilK](https://twitter.com/user/status/1292105911369191427) - 08.08.2020
-* [Used @simonw guide and did a self-updating profile README for GitHub. You'll find the guide at… https://t.co/IBtKzxyGvN](https://twitter.com/user/status/1291498173496791040) - 06.08.2020
-<!-- tweet ends -->
-Follow me [@soderlind](https://twitter.com/soderlind)
-</td></tr></table>
+More at [soderlind.no](https://soderlind.no/github/)
+
 
 <a href="https://github.com/soderlind/soderlind/actions"><img src="https://github.com/soderlind/soderlind/workflows/Build%20README/badge.svg" align="right" alt="Build README"></a>
-<a href="https://simonwillison.net/2020/Jul/10/self-updating-profile-readme/">How this works</a>
+
--- build_readme.py
+++ build_readme.py
@@ -13,209 +13,88 @@ client = GraphqlClient(endpoint="https://api.github.com/graphql")
 
 
 TOKEN = os.environ.get("SODERLIND_TOKEN", "")
-TWITTER_CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY", "")
-TWITTER_CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET", "")
-TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN", "")
-TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET", "")
 
 
 def replace_chunk(content, marker, chunk, inline=False):
-    r = re.compile(
-        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
-        re.DOTALL,
-    )
-    if not inline:
-        chunk = "\n{}\n".format(chunk)
-    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(
-        marker, chunk, marker)
-    return r.sub(chunk, content)
+	r = re.compile(
+		r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+		re.DOTALL,
+	)
+	if not inline:
+		chunk = "\n{}\n".format(chunk)
+	chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(
+		marker, chunk, marker)
+	return r.sub(chunk, content)
 
 
 def make_query(after_cursor=None):
-    return """
+	return """
 query {
   viewer {
-	repositories(first: 100, privacy: PUBLIC, after:AFTER) {
-	  pageInfo {
-		hasNextPage
-		endCursor
-	  }
+	repositories(first: 100, orderBy: {field:PUSHED_AT, direction:DESC}, privacy: PUBLIC, isFork: false,after:AFTER) {
+	  pageInfo {hasNextPage, endCursor}
 	  nodes {
 		name
 		description
+		pushedAt
 		url
-		releases(last:1) {
-		  totalCount
-		  nodes {
-			name
-			publishedAt
-			url
-		  }
-		}
+		forkCount
 	  }
 	}
   }
 }
 """.replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    )
+		"AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+	)
 
 
-def fetch_releases(oauth_token):
-    repos = []
-    releases = []
-    repo_names = set()
-    has_next_page = True
-    after_cursor = None
+def fetch_plugins(oauth_token):
+	plugins = []
+	has_next_page = True
+	after_cursor = None
 
-    while has_next_page:
-        data = client.execute(
-            query=make_query(after_cursor),
-            headers={"Authorization": "Bearer {}".format(oauth_token)},
-        )
-        print()
-        print(json.dumps(data, indent=4))
-        print()
-        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-                repos.append(repo)
-                repo_names.add(repo["name"])
-                releases.append(
-                    {
-                        "repo": repo["name"],
-                        "repo_url": repo["url"],
-                        "description": repo["description"],
-                        "release": repo["releases"]["nodes"][0]["name"]
-                        .replace(repo["name"], "")
-                        .strip(),
-                        "published_at": repo["releases"]["nodes"][0]["publishedAt"],
-                        "published_day": repo["releases"]["nodes"][0][
-                            "publishedAt"
-                        ].split("T")[0],
-                        "url": repo["releases"]["nodes"][0]["url"],
-                    }
-                )
-        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
-            "hasNextPage"
-        ]
-        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
-    return releases
-
-
-# def fetch_tils():
-#     sql = "select title, url, created_utc from til order by created_utc desc limit 5"
-#     return httpx.get(
-#         "https://til.simonwillison.net/til.json",
-#         params={"sql": sql, "_shape": "array",},
-#     ).json()
-def fetch_tweets():
-    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    api = tweepy.API(auth)
-    return [
-        {
-            "title": entry.text.translate({ord(c): None for c in '\n\t\r'}),
-            # "url": entry.entities["urls"][0]["expanded_url"],
-            # "url": ' '.join(url['url'] for url in entry.entities["urls"])
-           	"url": 'https://twitter.com/user/status/' + entry.id_str,
-            "published": entry.created_at.strftime('%d.%m.%Y'),
-        }
-		for entry in api.user_timeline('soderlind')
-    ]
-
-
-def fetch_read():
-    entries = feedparser.parse(
-        "https://getpocket.com/users/soderlind/feed/all")["entries"]
-    return [
-        {
-            "title": entry["title"],
-            "url": entry["link"].split("#")[0],
-            "published": time.strftime('%d.%m.%Y', entry["updated_parsed"]),
-        }
-        for entry in entries
-    ]
-
-
-def fetch_blog_entries():
-    entries = feedparser.parse("https://soderlind.no/feed.xml")["entries"]
-    return [
-        {
-            "title": entry["title"],
-            "url": entry["link"].split("#")[0],
-            "published": time.strftime('%d.%m.%Y', entry["updated_parsed"]),
-        }
-        for entry in entries
-    ]
+	while has_next_page:
+		data = client.execute(
+			query=make_query(after_cursor),
+			headers={"Authorization": "Bearer {}".format(oauth_token)},
+		)
+		print()
+		print(json.dumps(data, indent=4))
+		print()
+		i = 0
+		# for i, repo in enumerate(data["data"]["viewer"]["repositories"]["nodes"]):
+		for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+			if len(str(repo["description"])) > 4:
+				plugins.append(
+					{
+						"repo": repo["name"],
+						"url": repo["url"],
+						"font_format": "**" if i % 2 == 0 else "*",
+						"description": repo["description"].strip(),
+						"pushed_at": repo["pushedAt"],
+						"fork_count": repo["forkCount"],
+					}
+				)
+				i = i+1
+		has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
+			"hasNextPage"
+		]
+		after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
+	return plugins
 
 
 if __name__ == "__main__":
-    readme = root / "README.md"
-    project_releases = root / "releases.md"
-    releases = fetch_releases(TOKEN)
-    releases.sort(key=lambda r: r["published_at"], reverse=True)
-    # md = "\n".join(
-    #     [
-    #         "* [{repo} {release}]({url}) - {published_day}".format(**release)
-    #         for release in releases[:8]
-    #     ]
-    # )
-    # readme_contents = readme.open().read()
-    # rewritten = replace_chunk(readme_contents, "recent_releases", md)
+	readme = root / "README.md"
+	plugins = fetch_plugins(TOKEN)
+	plugins.sort(key=lambda r: r["pushed_at"], reverse=True)
+	md = "\n".join(
+		[
+			"[{font_format}{description}{font_format}]({url}) &nbsp;&nbsp;&nbsp;".format(
+				**plugin)
+			for plugin in plugins[:20]
+		]
+	)
+	readme_contents = readme.open().read()
+	rewritten = replace_chunk(readme_contents, "plugins", md)
 
-    entries = fetch_tweets()[:5]
-
-    tweet_md = "\n".join(
-        ["* [{title}]({url}) - {published}".format(**entry)
-         for entry in entries]
-    )
-    readme_contents = readme.open().read()
-    rewritten = replace_chunk(readme_contents, "tweet", tweet_md)
-
-    entries = fetch_read()[:5]
-    read_md = "\n".join(
-        ["* [{title}]({url}) - {published}".format(**entry)
-         for entry in entries]
-    )
-    rewritten = replace_chunk(rewritten, "read", read_md)
-
-    entries = fetch_blog_entries()[:5]
-    entries_md = "\n".join(
-        ["* [{title}]({url}) - {published}".format(**entry)
-         for entry in entries]
-    )
-    rewritten = replace_chunk(rewritten, "blog", entries_md)
-
-    readme.open("w").write(rewritten)
-
-# # Write out full project-releases.md file
-# project_releases_md = "\n".join(
-#     [
-#         (
-#             "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_day}\n"
-#             "<br>{description}"
-#         ).format(**release)
-#         for release in releases
-#     ]
-# )
-# project_releases_content = project_releases.open().read()
-# project_releases_content = replace_chunk(
-#     project_releases_content, "recent_releases", project_releases_md
-# )
-# project_releases_content = replace_chunk(
-#     project_releases_content, "release_count", str(len(releases)), inline=True
-# )
-# project_releases.open("w").write(project_releases_content)
-
-# tils = fetch_tils()
-# tils_md = "\n".join(
-#     [
-#         "* [{title}]({url}) - {created_at}".format(
-#             title=til["title"],
-#             url=til["url"],
-#             created_at=til["created_utc"].split("T")[0],
-#         )
-#         for til in tils
-#     ]
-# )
-# rewritten = replace_chunk(rewritten, "tils", tils_md)
+	readme.open("w").write(rewritten)
